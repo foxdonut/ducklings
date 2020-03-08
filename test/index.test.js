3eb9ad38f62ab112e9b49ab6a3fa67d64d43c034
foxdonut <fdaoud@fastmail.com>
2020-03-08 17:17:26 -0400

    ducklings: init

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,9 @@
-import { I, K } from '../src/index'
+import { G, I, K, Q, R, T, V } from '../src/index'
+
+const add = a => b => b + a
+const sub = a => b => b - a
+const mul = a => b => b * a
+const div = a => b => b / a
 
 export default {
   I: [
@@ -8,5 +13,17 @@ export default {
   K: [
     K(5)('anything'),
     5
+  ],
+  Q: [
+    T(10)(Q(add(11))(mul(5))(mul(2))(div(5))(G)),
+    42
+  ],
+  R: [
+    R(10)(add(11))(mul(5))(mul(2))(div(5))(G),
+    42
+  ],
+  V: [
+    V(2)(10)(sub),
+    8
   ]
 }
